8ce2419c7ad70ac5dd37700290698b38b6d1b680
Gianluca Miglietta <163427589+giallumigliet@users.noreply.github.com>
2026-08-16 14:46:02 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAOkre2FhmRFlSBPYznZUVAJxLQh-QeExc",
-  authDomain: "check-calendar-giallumigliet.firebaseapp.com",
-  projectId: "check-calendar-giallumigliet",
-  storageBucket: "check-calendar-giallumigliet.firebasestorage.app",
-  messagingSenderId: "741223614800",
-  appId: "1:741223614800:web:41af2763f7c3c6ebb5c455"
+
 };
 
 const app = initializeApp(firebaseConfig);
