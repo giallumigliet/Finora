c24227853f8422375bb8d2e75eb82db7e56dfb9d
Gianluca Miglietta <163427589+giallumigliet@users.noreply.github.com>
2026-08-18 20:09:00 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,15 @@
 // firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
 
 const firebaseConfig = {
-
+  apiKey: "AIzaSyBD3S_nOgf5nlq8yWdGwUOkWOP5q0RFdfg",
+  authDomain: "finora-17178.firebaseapp.com",
+  projectId: "finora-17178",
+  storageBucket: "finora-17178.firebasestorage.app",
+  messagingSenderId: "205225330212",
+  appId: "1:205225330212:web:2ffc287194742584eb7ab1"
 };
 
 const app = initializeApp(firebaseConfig);
